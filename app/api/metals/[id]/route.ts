04d2c74aeb99572cc3c437193eafe114ff0ed332
Vladislav Nikonov <vladdydaddy1029@gmail.com>
2025-07-05 19:39:17 +0300

    Прототип админ панели 2

--- app/api/metals/[id]/route.ts
+++ app/api/metals/[id]/route.ts
@@ -1,30 +1,35 @@
 import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(req: Request, {params }: { params: {id: string } } ) {
-	try {
-		const body = await req.json();
-		const { name, cashPrice, nonCashPrice} = body;
-		const updateMetal = await prisma.metal.update({
-			where: {id: params.id},
-			data: { name, cashPrice, nonCashPrice }
-		});
-		
-		return NextResponse.json(updateMetal)
-	} catch (error) {
-		
-	}
-};
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+  try {
+    const { params } = context;
+    const body = await req.json();
+    const { name, cashPrice, nonCashPrice } = body;
 
-export async function DELETE(_req: Request, { params }: { params: { id: string } }) {
-	try {
-		await prisma.metal.delete({
-			where: {id: params.id}
-		});
-		
-		return NextResponse.json({ message: "Deleted" });
-	} catch (error) {
-		console.error("Error deleting metal:", error);
-    	return NextResponse.json({ error: "Server error" }, { status: 500 });
-	}
+    const updatedMetal = await prisma.metal.update({
+      where: { id: params.id },
+      data: { name, cashPrice, nonCashPrice },
+    });
+
+    return NextResponse.json(updatedMetal);
+  } catch (error) {
+    console.error("PUT error:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(_req: NextRequest, context: { params: { id: string } }) {
+  try {
+    const { id } = context.params;
+
+    await prisma.metal.delete({
+      where: { id },
+    });
+
+    return NextResponse.json({ message: "Deleted" });
+  } catch (error) {
+    console.error("Error deleting metal:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
 }
