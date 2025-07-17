fa9c03c716edf5146765a25361613a113cf6f4f7
Vladislav Nikonov <vladdydaddy1029@gmail.com>
2025-07-17 16:49:12 +0300

    Исправление парамсов

--- app/api/categories/[id]/metals/route.ts
+++ app/api/categories/[id]/metals/route.ts
@@ -1,30 +1,34 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: {id: string } }) {
+interface Params {
+	params: {
+		id: string;
+	};
+}
+
+export async function GET(_: NextRequest, { params }: Params) {
 	try {
 		const category = await prisma.category.findUnique({
-			where: {id: params.id},
+			where: { id: params.id },
 			include: {
 				metals: {
 					include: {
-						metal: true
-					}
-				}
-			}
-		})
-		
+						metal: true,
+					},
+				},
+			},
+		});
+
 		if (!category) {
-			return NextResponse.json({ error: 'Категория не найдена' }, { status: 404 });
+			return NextResponse.json({ error: "Категория не найдена" }, { status: 404 });
 		}
-		
-		const metalList = category?.metals.map((m) => m.metal)
-		
-		return NextResponse.json(metalList)
+
+		const metalList = category.metals.map((m) => m.metal);
+
+		return NextResponse.json(metalList);
 	} catch (error) {
-		console.error('Ошибка при получении металлов по категории:', error);
-		return NextResponse.json({ error: 'Ошибка сервера' }, { status: 500 });
+		console.error("Ошибка при получении металлов по категории:", error);
+		return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
 	}
-	
-	
 }
