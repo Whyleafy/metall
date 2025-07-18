13dbdf1fa4387ba2f4904a577378ef47d2ac1283
Vladislav Nikonov <vladdydaddy1029@gmail.com>
2025-07-18 13:30:34 +0300

    feature: sticky header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,37 @@
-// Header.tsx
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
 import { Logo } from '@/components';
 import { Nav } from '@/components';
 import styles from './Header.module.scss';
 import { Phone } from 'lucide-react';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 export const Header = () => {
+	const [isHidden, setIsHidden] = useState(false)
+	
+	useEffect(() => {
+		let lastScrollY = window.scrollY;
+		const header = document.querySelector('.header');
+		
+		const updateHeader = () => {
+			const currentScrollY = window.scrollY;
+			
+			setIsHidden(currentScrollY > lastScrollY && currentScrollY > 100);
+			
+			lastScrollY = currentScrollY;
+		};
+		
+		window.addEventListener('scroll', updateHeader);
+		
+		return () => window.removeEventListener('scroll', updateHeader)
+	}, [])
+	
 	return (
-		<header className={styles.header}>
+		<header className={cn(styles.header, {
+				[styles.hidden]: isHidden,
+			})}>
 		<Link href="/" className={styles.header__link}>
 			<Logo />
 			<p className={styles.header__link__article}>Стальной двор</p>
