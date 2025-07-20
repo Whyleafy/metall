6e03baab9e141030d4570f19482e039641b9e4ba
Vladislav Nikonov <vladdydaddy1029@gmail.com>
2025-07-20 16:24:33 +0300

    fresh data price-list

--- app/price-list/page.tsx
+++ app/price-list/page.tsx
@@ -1,8 +1,9 @@
-import { Button, CategoryWithMetals, MetalTable, Title } from "@/components";
+import { Button, CategoryWithMetals, Title } from "@/components";
 import styles from './page.module.scss';
-import type { CategoryMetals } from "../../types/metals"
 import { getCategoriesWithMetals } from "../actions/getCategoryWithMetals";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 3600;
 
 export default async function PriceList() {
 	const categories = await getCategoriesWithMetals()
@@ -12,7 +13,6 @@ export default async function PriceList() {
 			<Title tag="h2" color="black">Таблица расценок </Title>
 			<div className={styles.priceList__buttons}>
 				{categories.map((category) => {
-				const isMain = category.name.toLowerCase() === "главная";
 				return (
 					
 					<Button sm={true} key={category.id} rounded={true} variant="outline" as="link" href={`#${category.name}`} >{category.name.trim().toLowerCase() === "главная" ? "Прием металлов по таблице" : `${category.name}`}</Button>
