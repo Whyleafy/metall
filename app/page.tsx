08214d9b18afb408321ceb76cb207c6e26f76b8d
Vladislav Nikonov <vladdydaddy1029@gmail.com>
2025-07-27 12:05:30 +0300

    yandex ver & metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+import { Metadata } from "next";
 import styles from "./page.module.scss";
 import { HeroSection, StepsSection, PriceSection, YandexSection, ContactSection, ReviewSection } from "@/components";
 
+export const metadata: Metadata = {
+	title: "Приём металлолома в Пущино — актуальные цены, вывоз, скупка | Металлолом Пущино",
+  description: "Приём металлолома в Пущино по высоким ценам. Актуальная таблица цен, приём черного и цветного лома. Адрес и контакты на карте.",
+	keywords: [
+    "прием металлолома Пущино",
+		"Металлалом пущино",
+    "скупка металла дорого",
+    "цены на металлолом",
+    "черный и цветной металл",
+    "прием лома Пущино",
+    "металлобаза",
+    "сдать металл",
+  ]
+}
 
 export default function Home() {
 	
