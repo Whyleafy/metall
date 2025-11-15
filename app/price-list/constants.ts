24d9e492297bc2fd3ca4f3375245888725acc137
Vladislav Nikonov <vladdydaddy1029@gmail.com>
2025-11-15 09:01:56 +0300

    new cable prices

--- app/price-list/constants.ts
+++ app/price-list/constants.ts
@@ -78,9 +78,9 @@ export const categories = [
     id: "cable",
     name: "Кабель",
     metals: [
-      { id: 1, name: "Кабель медный", price: 190 },
-      { id: 2, name: "Кабель медный луженый", price: 170 },
-      { id: 3, name: "Кабель алюминиевый", price: 40 },
+      { id: 1, name: "Кабель медный", price: 200 },
+      { id: 2, name: "Кабель медный луженый", price: 180 },
+      { id: 3, name: "Кабель алюминиевый", price: 50 },
     ]
   },
   {
